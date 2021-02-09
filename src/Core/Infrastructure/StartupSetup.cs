8aea7045aab4fce9c5866f9e2f19d7d38ebdbd75
ozenc.celik <ozenc.celik@inveon.com.tr>
2021-02-09 11:16:04 +0300

    Events removed.

--- src/Core/Infrastructure/StartupSetup.cs
+++ src/Core/Infrastructure/StartupSetup.cs
@@ -1,8 +1,13 @@
-﻿using Core.Services.Customers;
+﻿using Core.Events.Consumers;
+using Core.Services.Customers;
 using Data.Context;
 using Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Core.Infrastructure
 {
@@ -16,6 +21,9 @@ namespace Core.Infrastructure
 
         public static void RegisterDependencies(this IServiceCollection services)
         {
+            var consumers = GetConsumersToInject().ToList();
+            services.InjectEventConsumers(consumers);
+
             services.AddScoped(typeof(IRepository<>), typeof(EfCoreRepository<>));
 
             services.AddScoped<ICustomerService, CustomerService>();
@@ -28,6 +36,34 @@ namespace Core.Infrastructure
                 var context = serviceProvider.GetRequiredService<AppDbContext>();
                 context.Database.EnsureCreated();
                 Seeder.Initialize(serviceProvider);
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+
+        public static IEnumerable<Type> GetConsumersToInject()
+        {
+            var type = typeof(IConsumer<>);
+            var types = new List<Type>();
+            foreach (Type mytype in Assembly.GetExecutingAssembly().GetTypes()
+                .Where(mytype => mytype.GetInterfaces().Contains(type)))
+            {
+                types.Add(mytype);
+            }
+            /*
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => type.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+            */
+            return types;
+        }
+
+        private static void InjectEventConsumers(this IServiceCollection services, IEnumerable<Type> consumers)
+        {
+            foreach (var consumer in consumers)
+            {
+                services.AddSingleton(consumer, typeof(IConsumer<>));
             }
         }
         #endregion
