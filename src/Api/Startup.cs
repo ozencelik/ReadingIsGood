73a916722e697b42deb30e962ba8c425dc0dffbf
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2021-02-10 23:19:16 +0300

    swagger ui applied to release project not only development proect

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -139,9 +139,9 @@ namespace Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api " + ApiRoutes.Version));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api " + ApiRoutes.Version));
 
             app.UseHttpsRedirection();
 
